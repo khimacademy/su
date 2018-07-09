51ef51855c94d72f0147cd32036a7e755950f488
sarangkhim <sarang.khim@gmail.com>
2018-07-09 20:59:00 +0900

    ex

---
+++ ch4/4_11_c.py
@@ -0,0 +1,21 @@
+'''
+4-11. 내 피자, 네 피자
+연습문제 4-1로 프로그램을 시작하세요. 피자 리스트를 복사해서 friend_pizzas라고 저장하세요. 그리고 다음 작업을 하세요.
+- 원래 리스트에 새 피자를 추가하세요.
+- friend_pizzas 리스트에 다른 피자를 추가하세요.
+- 두 리스트가 별개임을 증명하세요. "내가 좋아하는 피자는:" 메시지를 출력하고 for 루프를 써서 첫 번째 리스트를 출력하세요. "내 친구가 좋아하는 피자는:" 메시지를 출력하고. for 루프를 써서 두 번째 리스트를 출력하세요. 새 피자가 각각 맞는 리스트에 저장됐는지 확인하세요.
+
+Output:
+My favorite pizzas are:
+- pepperoni
+- hawaiian
+- veggie
+- meat lover's
+
+My friend's favorite pizzas are:
+- pepperoni
+- hawaiian
+- veggie
+- pesto
+'''
+
